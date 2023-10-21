9595a8dec0c53dd3c240003be4597d4f6029f6ba
okonpatrik <okonpatrick2000@gmail.com>
2023-10-21 08:05:13 +0100

    this is a landing page sih atomic website

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,54 +1,33 @@
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 export default class Btn extends Component {
-    constructor(props){
-        super(props)
-    }
-    render() {
-        return (
-            <div class="py-md-5">
-            <div class="row"><div class="col-xl-6 col-lg-6 col-md-12">
-<div className="d-flex justify-content-between">
-      <Button variant="success" className="btn btn-sm px-3 btn-primary rounded-pill shadow-sm me-4">{this.props.Button1}</Button>
-     <Button variant="light" className="btn btn-sm px-3 btn-success rounded-pill shadow-sm me-4">{this.props.Button2}</Button>   
-     </div></div></div></div>
-
-      )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div class="py-md-5">
+        <div class="row">
+          <div class="col-xl-6 col-lg-6 col-md-12">
+            <div className="d-flex justify-content-between">
+              <Button
+                variant="success"
+                className="btn btn-sm px-3 btn-primary rounded-pill shadow-sm me-4"
+              >
+                {this.props.Button1}
+              </Button>
+              <Button
+                variant="light"
+                className="btn btn-sm px-3 btn-success rounded-pill shadow-sm me-4"
+              >
+                {this.props.Button2}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            {/* <div>
-            <div class="py-md-5">
-   <div class="container">
-   <div class="row"><div class="col-xl-6 col-lg-6 col-md-12">
-   <div class="mb-4 mb-xl-0 text-center text-md-start">
-      <div className="d-flex justify-content-between">
-      <Button variant="success" className="btn btn-sm px-3 btn-primary rounded-pill shadow-sm me-4">{this.props.Button1}</Button>
-     <Button variant="light" className="btn btn-sm px-3 btn-success rounded-pill shadow-sm me-4">{this.props.Button2}</Button>   
-     </div>
-     </div>
-     </div>
-     </div>
-     </div>
-     </div>
-            </div> */}
-  
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
